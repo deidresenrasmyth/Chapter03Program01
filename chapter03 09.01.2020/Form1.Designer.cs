eb6554dac55e43ce40979b7d8760409f604c4f1d
Deidre <deidre.balard@outlook.com>
2020-01-09 19:25:32 -0500

    Chapter03Program01

---
+++ chapter03 09.01.2020/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace chapter03_09._01._2020
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.butnCalc = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(102, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter first integer value";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(102, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter second integer value";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TxtOperand1
+            // 
+            this.TxtOperand1.Location = new System.Drawing.Point(567, 57);
+            this.TxtOperand1.Name = "TxtOperand1";
+            this.TxtOperand1.Size = new System.Drawing.Size(100, 20);
+            this.TxtOperand1.TabIndex = 2;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(567, 147);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand2.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(102, 218);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(565, 20);
+            this.txtResult.TabIndex = 4;
+            // 
+            // butnCalc
+            // 
+            this.butnCalc.Location = new System.Drawing.Point(102, 287);
+            this.butnCalc.Name = "butnCalc";
+            this.butnCalc.Size = new System.Drawing.Size(75, 23);
+            this.butnCalc.TabIndex = 5;
+            this.butnCalc.Text = "&Calculate";
+            this.butnCalc.UseVisualStyleBackColor = true;
+            this.butnCalc.Click += new System.EventHandler(this.ButnCalc_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(601, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.butnCalc);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.TxtOperand1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Interger Division";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtOperand1;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button butnCalc;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ chapter03 09.01.2020/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter03_09._01._2020
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ButnCalc_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            decimal operand1;
+            decimal operand2;
+            decimal answer;
+            //Input Step
+            //Check first input...
+            flag = decimal.TryParse(TxtOperand1.Text, out operand1);
+            if (flag == false)
+            {
+                MessageBox.Show("Enter a whole number", "Input Error");
+                TxtOperand1.Focus();
+                return;
+            }
+                // Check second input...
+            flag = decimal.TryParse(txtOperand2.Text, out operand2);
+            if (flag == false)
+            {
+                MessageBox.Show("Enter a whole number", "Input Error");
+                txtOperand2.Focus();
+                return;
+             }
+            //Process Step
+            answer = operand1 / operand2;
+            // Display Step
+            txtResult.Text = operand1.ToString() + "divided by" +
+                operand2.ToString() +
+                "equals" + answer.ToString();
+            txtResult.Visible = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Close();
+                    }
+    }
+}
